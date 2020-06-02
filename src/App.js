a32c37faa8e5e79b18a7ded0f7bbc0d2a4823d6a
kjgarza <kgarza@datacite.org>
2020-06-03 00:24:14 +0200

    docs: front

--- src/App.js
+++ src/App.js
@@ -34,61 +34,71 @@ class App extends Component {
           <small>Check the code in <a href="https://github.com/kjgarza/talk-react-d3">https://github.com/kjgarza/talk-react-d3</a></small>
         </div>
         <Grid>
+
+        <Article col='1-5'></Article>
+
+        <Article col="3-5">
+        <h2 id="uptake">Uptake</h2>
+<img src="https://i.imgur.com/nsrmbfD.png" alt="Comparison"></img>
+<p>In terms of uptake, the libraries have marked differences. Recharts is leading the way to this area. Vega and Nivo
+  are very close and in second place in this area. Semiotic and Brightchart fall at the bottom in terms of uptake, but
+  they are also newer. Rechart it&#39;s even the most used library with around 27K installs, the closest competitor is
+  Vega with 1.5K install. The other libraries way behind the others. Contributors wise Rechart is also leading with 143
+  contributors, and again Vega follows second with 91 contributors. In summary, we could say Rechart is the most popular
+  library out there with Vega following second. All the others are not broadly used.</p>
+<blockquote>
+  <p>Runner-up: Vega</p>
+  <p>Winner: Recharts</p>
+</blockquote>
+<h2 id="api-customization">API customization</h2>
+<p>All the libraries offer good API to create charts. Nivo and Vega seem to have the larger are more customizable APIs.
+  Nivo had the disadvantage (In my experience) of making it too difficult to make a simple bar chart. That is not
+  something that happens with any of all the other libraries. In terms of features, one big difference in the Vega API
+  is that it offers API that the other libraries do not include. For example, it has Data API for data transformation
+  (very much like <code>dplyr</code>), which is very useful to manipulate the data before visualization without
+  depending on extra code. It also includes a Layout API, which can be used to compose many charts together.</p>
+<blockquote>
+  <p>Runner-up: Recharts, Semiotic, Brithcarts</p>
+  <p>Winner: Vega</p>
+</blockquote>
+<h2 id="approach">Approach</h2>
+<p>All the libraries use a declarative approach to create visualizations. In declarative visualization, you describe the
+  graphic that you want. This description does not correspond with a chart type; instead, you define the various
+  entities that should make up the chart and the relationships between those entities. This ultimately is what it means
+  to be declarative: describing what your chart should show, not how it should be created. All the libraries achieve by
+  creating a react object to declare the chart visualization (see demo examples). However, two libraries have
+  distinguishing characteristics. Recharts takes the declarative approach a step further as it creates components for
+  each element of the visualization, so it uses a Declarative components approach. On the other hand, Vega does
+  something slightly different, as it uses a schema to define the chart visualization. This schema can be exported to
+  JSON and used anywhere else using the Vega runtime.</p>
+<blockquote>
+  <p>Runner-up: all others</p>
+  <p>Winner: Recharts and Vega </p>
+</blockquote>
+<h2 id="compatibility-with-another-datacite-tooling">Compatibility with another DataCite tooling</h2>
+<p>Another Datacite tooling here refers to two tools: Jupyter Notebooks and React. The only two libraries that
+  successfully check this box are Semiotic and Vega. The rest do not have Jupyetr notebook support.</p>
+<blockquote>
+  <p>Winner: Semiotic and Vega </p>
+</blockquote>
+<h2 id="documentation-and-support">Documentation and Support</h2>
+<p>In terms of development aid, all libraries are written in Typescript (except for Nivo), which is useful for the
+  development as sone of them does have a complex API. </p>
+<p>All the libraries have a fair amount of documentation. In terms of examples that one can reuse, I do think that Vega
+  provides more samples to look at thanks to a large number of vega specifications available in JSON format.</p>
+<p>Only Vega provides a <a href="https://vega.github.io/editor/#/examples/vega/bar-chart">online playground</a>, where
+  the charts can be edited in isolation, this is actually very useful to isolate the visualization from React or your
+  application during development.</p>
+<blockquote>
+  <p>Winner: Vega </p>
+</blockquote>
+
+
+        </Article>
        
 
-        <Article col='1-2'
-            className="uk-margin-bottom"
-            title="Uptake and popularity"
-            lead=""
-          >
-            <section className="demo demo-recharts uk-margin-bottom">
-            <img src="https://i.imgur.com/nsrmbfD.png" />
-            </section>
-          </Article>
-
-          <Article col='1-2'
-            className="uk-margin-bottom"
-            title="Uptake and popularity"
-            lead=""
-          >
-            <section className="demo demo-recharts uk-margin-bottom">
-            <p  align="left">
-              In terms of uptake the libraries have marked differences. Recharts is clearly leading the way on this area. 
-              
-              Vega and Nivo are very close and in second place on this area.
-
-              Semiotic and Brightchart fall down at the bottom in terms of uptake, but they are also newer.
 
 
-              Rechart its also the most used library with around 27K installs, the closer competitor is Vega with 1.5K install. The other libraries way behind the others.
-
-              Constributors wise Rechart is also leading with 143 contributors, again Vega follows second with 91 constributos. 
-
-              In summary we could say Rechart is the most popular library out there with Vega following second. All the others are not broadly used.
-
-            </p>
-            </section>
-          </Article>
-
-          <Article col='1-3'></Article>
-
-          <Article col='1-3'
-            className="uk-margin-bottom"
-            title="Similarities"
-            lead=""
-          >
-            <section className="demo demo-recharts uk-margin-bottom">
-            <p  align="left">
-            All the libraries are very similar in certain aspects. They all have react wrapper and the all offer DOM interaction for the visulisation.
-            In terms of development aid they all are writing in Typescript (with exception of Nivo) which is useful for the development as sone of them do have a complex API.
-            All the libraries also use a declarative approach.    
-            In declarative visualization, you provide a description of the graphic that you want. This description does not correspond with a chart type; rather, you describe the various entities that should make up the chart and the relationships between those entities. These entities might include:
-            This is ultimately what it means to be declarative: describing what your chart should show, not how it should be created.
-
-
-            </p>
-            </section>
-          </Article>
 
           <Article col='1-2'
             className="uk-margin-bottom"
@@ -105,9 +115,6 @@ class App extends Component {
             title= "Recharts"
           >
             <section className="demo demo-recharts uk-margin-bottom">
-            <p>All the components of Recharts are clearly separated. The lineChart is composed of x axis, tooltip, grid, and line items, and each of them is an independent React Component. The clear separation and composition of components is one of the principle Recharts follows.
-
-</p>
 
             <p  align="left">
             Differences:
@@ -135,7 +142,6 @@ class App extends Component {
             title= "Nivo"
           >
             <section className="demo demo-recharts uk-margin-bottom">
-            <p>Line chart demos with different React D3 libraries: Victory.js, Recharts, Nivo, VX, Britecharts-react and react-vis.</p>
             <br/>
             <ul align="left">
             <li>Has a comprehensive and customizible API. but a bit confusing</li>
@@ -160,13 +166,7 @@ class App extends Component {
             lead=""
           >
             <section className="demo demo-recharts uk-margin-bottom">
-            <p  align="left">
-
-              A Vega specification defines an interactive visualization in a JSON format. Specifications are parsed by Vega’s JavaScript runtime to generate both static images or interactive web-based views. Vega provides a convenient representation for computational generation of visualizations, and can serve as a foundation for new APIs and visual analysis tools.
-
-
-
-            </p>
+        
             <br/>
             <p  align="left">
               Differences:
@@ -196,11 +196,7 @@ class App extends Component {
             lead=""
           >
             <section className="demo demo-recharts uk-margin-bottom">
-            <p>
-            It provides three types of frames XYFrame, OrdinalFrame, NetworkFrame, to deploy a wide variety of charts.
-
-
-            </p>
+            
 
 
             <br/>
@@ -237,9 +233,7 @@ class App extends Component {
             title= "Britecharts-React"
           >
             <section className="demo demo-recharts uk-margin-bottom">
-            <p  align="left">
-              Britecharts-react proposes a way of integrating Britecharts and React that promotes a clear separation of concerns between the libraries and aims for easy and effortless maintenance of the charts.
-            </p>
+      
             <br/>
             <p  align="left">
             Differences:
@@ -253,21 +247,18 @@ class App extends Component {
             </section>
           </Article>
 
-          <Article col='1-3'></Article>
-          <Article col='1-3'
+          <Article col='1-5'></Article>
+          <Article col='3-5'
             className="uk-margin-bottom"
             title="Summary and suggestion"
             lead=""
           >
             <section className="demo demo-recharts uk-margin-bottom">
             <p  align="left">
-            I think there are three groups. 
-            The first groups is composed by Britecharts and Semiotic, these two libraries have a medium size and pretty similar APIs but they lack of popularity and updtake compared to rest.
-            The second group is comformed by Vega and Nivo, they are the kind of the opposite to the first group: way more popular and extensive and customizible API.
-            The third group includes only Recharts the most popular of all, it has  medium size API and it also the only one that uses a component base approach to build the charts.
+            I have managed to create simple visualizations with all the libraries, which show that they are all excellent libraries. However, in terms of looking for the best, I think that one would find Recharts and, especially, Vega's shining brighter than the other libraries.
 
-            My suggestion is that we choose a library from the Second or third group. I would discard Nivo as their API seems extreamly complex, so even making a simple chart its difficult.
-            Therefore we should probably choose between Vega and Recharts.
+My suggestion is to decide for either Recharts or Vega as the charting library to use.
+  Therefore we should probably choose between Vega and Recharts.
             </p>
             </section>
           </Article>
